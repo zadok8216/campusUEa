c563aaec52da35d98df7fbe49e6aee882baff497
ViniciosCunha <vpc.hef@uea.edu.br>
2020-06-17 05:09:21 -0300

    commit segunda tela

---
+++ app/src/main/java/com/example/campusuea/infoUniversidades.kt
@@ -0,0 +1,11 @@
+package com.example.campusuea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class infoUniversidades : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_universidades)
+    }
+}
